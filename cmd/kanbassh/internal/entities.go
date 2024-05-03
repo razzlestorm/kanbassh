c960994fc9618f321a1698570fd659d2d8d1561f
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2024-05-02 22:47:28 -0700

    flesh out struct definitions

--- cmd/kanbassh/internal/entities.go
+++ cmd/kanbassh/internal/entities.go
@@ -1,9 +1,31 @@
 package entities
 
-import ()
+import (
+	"time"
+
+	"github.com/gliderlabs/ssh"
+	"golang.org/x/term"
+)
 
 type Board struct {
+	Name    string
+	Columns []Column
+	Users   []User
+}
+
+type Column struct {
+	Name    string
+	Tickets []Ticket
 }
 
 type Ticket struct {
+	Name        string
+	Due_Date    time.Time
+	Description string
+	Assignee    User
+}
+
+type User struct {
+	Session  ssh.Session
+	Terminal *term.Terminal
 }
