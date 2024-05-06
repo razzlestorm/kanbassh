d9590711894e83febb978c3301a4351ae7491077
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2024-05-05 23:24:58 -0700

    more commands

--- cmd/kanbassh/cli/cli.go
+++ cmd/kanbassh/cli/cli.go
@@ -9,8 +9,11 @@ import (
 var (
 	createBoard = regexp.MustCompile(`^/create_board.*`)
 	joinBoard = regexp.MustCompile(`^/join.*`)
+	listBoards = regexp.MustCompile(`^/list.*`)
 	editBoard = regexp.MustCompile(`^/edit_board.*`)
 	createTicket = regexp.MustCompile(`^/create_ticket.*`)
+	listTickets = regexp.MustCompile(`^/list_tickets.*`)
 	editTicket = regexp.MustCompile(`^/edit_ticket.*`)
 	moveTicket = regexp.MustCompile(`^/move_ticket.*`)
 )
+
