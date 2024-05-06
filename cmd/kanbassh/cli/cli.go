d1b6ca69146e5534d1b64902a70447abea133414
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2024-05-05 23:19:27 -0700

    add placeholder commands

--- cmd/kanbassh/cli/cli.go
+++ cmd/kanbassh/cli/cli.go
@@ -1,1 +1,16 @@
 package cli
+
+import (
+	"regexp"
+	"github.com/razzlestorm/kanbassh/internal/entities"
+)
+
+
+var (
+	createBoard = regexp.MustCompile(`^/create_board.*`)
+	joinBoard = regexp.MustCompile(`^/join.*`)
+	editBoard = regexp.MustCompile(`^/edit_board.*`)
+	createTicket = regexp.MustCompile(`^/create_ticket.*`)
+	editTicket = regexp.MustCompile(`^/edit_ticket.*`)
+	moveTicket = regexp.MustCompile(`^/move_ticket.*`)
+)
